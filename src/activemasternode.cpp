d7dab3e283773357a52268a1a721f366fe65e33b
TSC <36497576+temp69@users.noreply.github.com>
2018-02-24 09:24:02 +0100

    fix "notCapableReason" for coldwallet setup
    Masternode started remotly showed: {"notCapableReason" : "Could not find suitable coins!"}.

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -488,6 +488,7 @@ bool CActiveMasternode::EnableHotColdMasterNode(CTxIn& newVin, CService& newServ
     if(!fMasterNode) return false;
 
     status = MASTERNODE_REMOTELY_ENABLED;
+    notCapableReason = "";	
 
     //The values below are needed for signing dseep messages going forward
     this->vin = newVin;
